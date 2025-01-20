35548e2095d6b27cddadd7be325b2e641df3c70a
dengyakui <dengyakui@outlook.com>
2025-01-20 15:42:14 +0800

    Add project files.

---
+++ VSIXProject1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSIXProject1
+{
+    /// <summary>
+    /// Helper class that exposes all GUIDs used across VS Package.
+    /// </summary>
+    internal sealed partial class PackageGuids
+    {
+        public const string guidVSIXProject1PackageString = "641260e7-1b62-4d3e-a8a5-243409f90935";
+        public static Guid guidVSIXProject1Package = new Guid(guidVSIXProject1PackageString);
+
+        public const string guidVSIXProject1PackageCmdSetString = "02560bbd-a5ff-4c2b-a768-443a890d68d2";
+        public static Guid guidVSIXProject1PackageCmdSet = new Guid(guidVSIXProject1PackageCmdSetString);
+    }
+    /// <summary>
+    /// Helper class that encapsulates all CommandIDs uses across VS Package.
+    /// </summary>
+    internal sealed partial class PackageIds
+    {
+        public const int MyMenuGroup = 0x1020;
+        public const int Command1Id = 0x0100;
+    }
+}
